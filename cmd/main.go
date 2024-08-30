c7be29a67bde158bb4d512088904ed8b9873096f
Fabio Vige <fabiovige@gmail.com>
2024-08-30 20:11:24 -0300

    implementado rota responsaveis

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,22 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/fabiovige/golang-maieutica-backend/controller"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	server := gin.Default()
+
+	ResponsavelController := controller.NewResponsavelController()
 
 	server.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
+	server.GET("/responsaveis", ResponsavelController.GetResponsaveis)
 
 	server.Run(":8000")
 }
