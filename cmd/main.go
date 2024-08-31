25251fe35cc1d38afc19977d94cb8e427d7a996b
Fabio Vige <fabiovige@gmail.com>
2024-08-30 21:06:07 -0300

    listando responsaveis

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,29 @@ package main
 
 import (
 	"github.com/fabiovige/golang-maieutica-backend/controller"
+	"github.com/fabiovige/golang-maieutica-backend/db"
+	"github.com/fabiovige/golang-maieutica-backend/repository"
+	"github.com/fabiovige/golang-maieutica-backend/usecase"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	server := gin.Default()
 
-	ResponsavelController := controller.NewResponsavelController()
+	// conexao com o banco
+	dbConn, err := db.ConnDB()
+	if err != nil {
+		panic(err)
+	}
+
+	// camada repository
+	responsavelRepository := repository.NewResponsavelRepository(dbConn)
+
+	// camada usecase
+	responsavelUseCase := usecase.NewResponsavelUseCase(responsavelRepository)
+
+	// camadas de controller
+	ResponsavelController := controller.NewResponsavelController(responsavelUseCase)
 
 	server.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
